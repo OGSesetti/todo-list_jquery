db878c7d161e23eee42ce310d33be6c1af500b06
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-05-20 21:36:31 +0300

    dsasa

--- js/script.js
+++ js/script.js
@@ -90,5 +90,30 @@ function clearCompleted() {
 }
 
 */
+(function ($) {
 
-$(addTask)
+
+})
+
+
+
+$("#addTask").click(function () {
+    var task = $("#textInput").val();
+    if (task !== "") {
+        $("#theList").append("<li>" + task + ' <button class="complete">Merkitse tehdyksi</button></li>');
+        $("#textInput").val("");
+
+    }
+});
+$("#nuke").click(function () {
+    $("#theList").empty();
+});
+
+$(document).on("click", ".complete", function () {
+    $(this).parent().toggleClass("completed")
+});
+
+
+$("#clearCompleted").click(function () {
+    $("#theList .completed").remove();
+})
